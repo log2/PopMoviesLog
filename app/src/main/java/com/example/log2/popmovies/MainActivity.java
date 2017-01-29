297fba7c2f2073e7699df6e13b10bce564d76c7f
Lorenzo Gallucci <lgallucci@gmail.com>
2017-01-29 22:27:23 +0100

    Removed unwanted FloatingActionButton

--- app/src/main/java/com/example/log2/popmovies/MainActivity.java
+++ app/src/main/java/com/example/log2/popmovies/MainActivity.java
@@ -4,15 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.android.volley.Response;
 
@@ -48,14 +45,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         recyclerView = (RecyclerView) findViewById(R.id.rv_movies);
         recyclerView.setHasFixedSize(true);
